6981da01a63d2fcf168e787cf31e77197cc02c25
zhuwei <zhuwei_leo@163.com>
2017-04-30 16:40:23 +0800

    完成about

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,9 +3,11 @@
   <div class="holder">
     <h2>About<br></h2>
     <div class="tabs">
-      <div class="tab"><a href="">profile</a><a href="">skills</a></div>
+      <div class="tab">
+        <a v-for="item in items" :class="{active:selected===item}" href="" @click.prevent="selected=item">{{item}}</a>
+      </div>
       <div class="tab-content">
-        <div class="profile">
+        <div v-show="selected === 'profile'" class="profile">
           <p>I'm Rakesh Dubai based UI/UX/Web Designer and Front-End Developer.</p>
           <p>I am able to work effectively in a team or on my Own. I have demonstrated an ability to lead small Teams and mentor other workers. I pride myself on being able to use my knowledge and initiative to deliver high quality results to tight deadlines. I enjoy a challenge and look for the opportunity to expand and develop my skill set.</p>
           <p>Some people call what I do User Experience Design, some call it Web design, some think I am a Front-End Developer, others see me as a UI Designer. I see what I do as a combination of all these things and ultimately don’t think it really matters which label you like to use.</p>
@@ -14,7 +16,36 @@
             <a class="button" href="">Download PDF</a><a class="button" href="">Hire me</a>
           </div>
         </div>
-        <div class="skills"></div>
+        <div v-show="selected ==='skills'" class="skills">
+          <div class="html">
+            <!--<span>html</span>-->
+            <div>html</div>
+          </div>
+          <div class="css">
+            <!--<span>css</span>-->
+            <div>css</div>
+          </div>
+          <div class="javascript">
+            <!--<span>javascript</span>-->
+            <div>javascript</div>
+          </div>
+          <div class="jquery">
+            <!--<span>jquery</span>-->
+            <div>jquery</div>
+          </div>
+          <div class="vue">
+            <!--<span>vue</span>-->
+            <div>vue</div>
+          </div>
+          <div class="webpack">
+            <!--<span>webpack</span>-->
+            <div>webpack</div>
+          </div>
+          <div class="photoshop">
+            <!--<span>photoshop</span>-->
+            <div>photoshop</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -23,10 +54,17 @@
 </template>
 <script>
 export default {
-    name:"About"
+    name:"About",
+    data:function (){
+        return {
+            selected:"profile",
+            items:["profile","skills"]
+        }
+    }
 }
 </script>
 <style lang="scss">
+$text-active-c:#14b97d;
 #About{
   &:before{
     content:"";
@@ -50,6 +88,20 @@ export default {
   }
   .tabs{
     .tab{
+      margin-top:20px;
+      .active{
+        color: $text-active-c;
+        &:after{
+          background: $text-active-c;
+          content:"";
+          display:block;
+          height:2px;
+          width:100%;
+          margin:0 auto;
+          transition:all .5s;
+          margin-top:2px;
+        }
+      }
       a{
         font-size:30px;
         font-weight:600;
@@ -74,6 +126,7 @@ export default {
     }
     .tab-content{
       width:60%;
+      height:80vh;
       margin:0 auto;
       .profile{
         p{
@@ -90,6 +143,34 @@ export default {
             &:hover{color:white}
           }
         }
+      }
+      .skills{
+        margin-top:20px;
+        >div{
+          display:flex;
+          justify-content:space-between;
+          background: $text-active-c;
+          height:25px;
+          border-radius:3px;
+          margin-bottom: 20px;
+          line-height:25px;
+          text-align:justify;
+          padding-left:10px;
+          color:white;
+          font-weight:700;
+          &:after{
+            position:relative;
+            right:-3em;
+            color:black;
+          }
+        }
+        .html{width:90%;&:after{content:"90%"}}
+        .css{width:80%;&:after{content:"80%"}}
+        .javascript{width:85%;&:after{content:"85%"}}
+        .jquery{width:80%;&:after{content:"80%"}}
+        .vue{width:70%;&:after{content:"70%"}}
+        .webpack{width:60%;&:after{content:"60%"}}
+        .photoshop{width:50%;&:after{content:"50%"}}
       }
     }
   }
---
+++ src/components/Work.vue
@@ -0,0 +1,40 @@
+<template>
+<section id="Work">
+  <h2>Works</h2>
+  <div class="holder">
+    <div class="resumer">
+      <a href="">
+        <div class="bg"></div>
+        <div class="discrib">
+          <h3>Resumer</h3>
+          <p>在线简历生成器</p>
+        </div>
+      </a>
+    </div>
+    <div class="Tips">
+      <a href="">
+        <div class="bg"></div>
+        <div class="discrib">
+          <h3>Tips</h3>
+          <p>便利贴应用</p>
+        </div>
+      </a>
+    </div>
+    <div class="Music">
+      <a href="">
+        <div class="bg"></div>
+        <div class="discrib">
+          <h3>Music</h3>
+          <p>在线音乐播放器</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</section>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
