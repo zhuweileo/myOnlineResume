8300fa37d38cd995f21fcebcef0d3bd56b44e3a9
zhuwei <zhuwei_leo@163.com>
2017-05-02 11:57:09 +0800

    修改

--- src/components/About.vue
+++ src/components/About.vue
@@ -13,7 +13,7 @@
             <p>选择前端的原因：最初是因为我喜欢排版，喜欢把杂乱的东西变得井井有条。然后发现前端并不都是排版啊！哈哈，但是依然热爱！</p>
             <p>有半年的前端开发经验，熟悉html/css/javascript，熟悉W3C标准，熟练使用Jquery等常用库，有一定的<strong>Vuejs</strong>使用经验，并且熟练使用webpack等前端工具；
 了解ajax/http/BOM/DOM等前端相关知识；对js<strong>模块化</strong>、<strong>面向对象编程</strong>有一定理解；对<strong>Html+css布局</strong>工作较为敏感。</p>
-            <p>我是一位性格幽默，做事认真的人。喜欢学习心得技术，良好的沟通理解能力，有一定独立解决问题的能力，而且有时间观念，能够在规定的时间内完成任务。</p>
+            <p>我是一位性格幽默，做事认真的人。喜欢学习新技术，有极客精神，良好的沟通理解能力，有一定独立解决问题的能力，而且有时间观念，能够在规定的时间内完成任务。</p>
             <p>我希望能得到一份<strong>初级前端工程师</strong>的工作，如果您觉得我是你要找的那个人请联系我！</p>
             <div class="btns">
               <a class="button" href="/myOnlineResume/src/assets/pdf/resume.pdf" target="_blank">Download PDF</a><a class="button" href="mailto:zhuwei_leo@163.com">Hire me</a>
