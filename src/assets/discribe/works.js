f7a31178de2f9ad2dc201a4da9e1ca7e2be05bbc
zhuwei <zhuwei_leo@163.com>
2017-05-08 15:23:48 +0800

    添加三个项目

---
+++ src/assets/discribe/works.js
@@ -0,0 +1,13 @@
+/**
+ * Created by 维 on 2017/5/8.
+ */
+export default [
+  {url: "https://zhuweileo.github.io/resume/dist", h3: "Resumer", p: "在线简历生成器"},
+  {url: "http://zhuweileo.top", h3: "Tips", p: "便利贴应用"},
+  {url: "http://zhuweileo.coding.me/music/", h3: "Music", p: "在线音乐播放器"},
+  {url: "https://zhuweileo.github.io/lunbo/index.html", h3: "carousel", p: "无限轮播插件"},
+  {url: "https://zhuweileo.github.io/todoList/page.html", h3: "TodoLsit", p: "TodoList在线应用"},
+  {url: "https://jirengu-inc.github.io/jrg-vip10/homework/%E6%9C%B1%E7%BB%B4/20161223/", h3: "WarCraft", p: "魔兽首页"},
+  // {url: "", h3: "", p: ""},
+]
+
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,108 +1,118 @@
 <template>
-<section id="Work">
-  <h2>Works</h2>
-  <div class="holder">
-    <div class="pro">
-      <a href="https://zhuweileo.github.io/resume/dist" target="_blank">
-        <div class="bg resumer"></div>
-        <div class="discribe">
-          <h3>Resumer</h3>
-          <p>在线简历生成器</p>
-        </div>
-      </a>
+  <section id="Work">
+    <h2>Works</h2>
+    <div class="holder">
+      <div class="pro" v-for="item in works">
+        <a :href="item.url" target="_blank">
+          <div class="bg"></div>
+          <div class="discribe">
+            <h3>{{item.h3}}</h3>
+            <p>{{item.p}}</p>
+          </div>
+        </a>
+      </div>
     </div>
-    <div class="pro">
-      <a href="http://zhuweileo.top" target="_blank">
-        <div class="bg tips"></div>
-        <div class="discribe">
-          <h3>Tips</h3>
-          <p>便利贴应用</p>
-        </div>
-      </a>
-    </div>
-    <div class="pro">
-      <a href="http://zhuweileo.coding.me/music/" target="_blank">
-        <div class="bg music"></div>
-        <div class="discribe">
-          <h3>Music</h3>
-          <p>在线音乐播放器</p>
-        </div>
-      </a>
-    </div>
-  </div>
-</section>
+  </section>
 </template>
 <script>
-
-</script>
-<style lang="scss">
-$text-active-c:#14b97d;
-#Work{
-  background: #000;
-  padding-bottom:20vh;
-  &:before{
-    content:"";
-    display:block;
-    height:12vh;
-    background: #000;
-    transform:skewY(-3deg);
-    transform-origin:0 0;
-  }
-  h2{
-    font-size:50px;
-    color:white;
-    font-weight:700;
-    text-transform: uppercase;
-    margin-bottom:20px;
-    &:after{
-      content:"";
-      display:block;
-      height:3px;
-      width:1.5em;
-      background: $text-active-c;
-      margin:0 auto;
+  import works from "../assets/discribe/works"
+  export default {
+    name: "Work",
+    data(){
+        return {
+            works:works
+        }
     }
   }
-  .holder{
-    display:flex;
-    justify-content: center;
-    .pro{
-      padding:50px 0;
-      &:first-child a{background-image:url(../assets/img/resumer.jpg)}
-      &:nth-child(2) a{background-image:url(../assets/img/tips.jpg)}
-      &:nth-child(3) a{background-image:url(../assets/img/music.jpg)}
-      a{
-        background-position:center;
-        background-repeat: no-repeat;
-        background-size:cover;
-        display:block;
-        width:30vw;
-        height:35vh;
-        margin-right:10px;
-        position:relative;
-        &:hover{
-          .discribe{opacity:1;}
+</script>
+<style lang="scss">
+  $text-active-c: #14b97d;
+  #Work {
+    background: #000;
+    padding-bottom: 20vh;
+    &:before {
+      content: "";
+      display: block;
+      height: 12vh;
+      background: #000;
+      transform: skewY(-3deg);
+      transform-origin: 0 0;
+    }
+    h2 {
+      font-size: 50px;
+      color: white;
+      font-weight: 700;
+      text-transform: uppercase;
+      margin-bottom: 100px;
+      &:after {
+        content: "";
+        display: block;
+        height: 3px;
+        width: 1.5em;
+        background: $text-active-c;
+        margin: 0 auto;
+      }
+    }
+    .holder {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+      /*width: 100%;*/
+      min-width:30vw;
+      .pro {
+        &:first-child a {
+          background-image: url(../assets/img/resumer.jpg)
         }
-        .discribe{
-          display:flex;
-          justify-content: center;
-          flex-direction:column;
-          background: $text-active-c;
-          color:white;
-          position:absolute;
-          top:0;
-          bottom:0;
-          left:0;
-          right:0;
-          opacity:0;
-          transition:all .8s;
-          h3{
-            font-size:20px;
-            font-weight:700;
+        &:nth-child(2) a {
+          background-image: url(../assets/img/tips.jpg)
+        }
+        &:nth-child(3) a {
+          background-image: url(../assets/img/music.jpg)
+        }
+        &:nth-child(4) a {
+          background-image: url(../assets/img/lunbo.png)
+        }
+        &:nth-child(5) a {
+          background-image: url(../assets/img/ToDoList.jpg)
+        }
+        &:nth-child(6) a {
+          background-image: url(../assets/img/warcraft.jpg)
+        }
+        a {
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+          display: block;
+          width: 30vw;
+          height: 35vh;
+          margin-right: 10px;
+          margin-bottom:20px;
+          position: relative;
+          &:hover {
+            .discribe {
+              opacity: 1;
+            }
+          }
+          .discribe {
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            background: $text-active-c;
+            color: white;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            opacity: 0;
+            transition: all .8s;
+            h3 {
+              font-size: 20px;
+              font-weight: 700;
+            }
           }
         }
       }
     }
   }
-}
 </style>
